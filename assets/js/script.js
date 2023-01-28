4b577b9ea48386ac0d57ef1970fafbc8f3bf25e0
Mario <mario.cali@gmail.com>
2023-01-28 18:26:46 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,7 +81,7 @@ if (inputBox !== null) {
         inputBox.value = searchQuery || "";
         executeSearch(searchQuery, false);
     } else {
-        document.getElementById('search-results').innerHTML = '<p class="search-results-empty">Please enter a word or phrase above, or see <a href="/tags/">all tags</a>.</p>';
+        document.getElementById('search-results').innerHTML = '<p class="search-results-empty">Inserisci una parola o una frase qui sopra, oppure vedi <a href="/tags/">tutti i tags</a>.</p>';
     }
 }
 
